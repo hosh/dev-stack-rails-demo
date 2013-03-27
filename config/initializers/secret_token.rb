85bbf7057e27f8b88d78b3f59ac687b8af8109b6
Ho-Sheng Hsiao <hosh@opscode.com>
2013-03-27 12:44:35 -0700

    [ARCH] rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DevStackRailsDemo::Application.config.secret_token = '52bc26e62235365f1b285e779ff36c4f6b039d2d1536a7e38f51e3d10d0b507df09a471c029adab5ade6d2a6fc4887fc1ebd831eefadceb712127f847cf82329'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DevStackRailsDemo::Application.config.session_store :cookie_store, key: '_dev-stack-rails-demo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DevStackRailsDemo::Application.config.session_store :active_record_store
